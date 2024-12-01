d65de6c62d949f018a1b6aedb545bd04f3edd0a2
Anna Luísa <annalf1401@gmail.com>
2024-11-30 23:29:20 -0300

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,22 @@
-const hamburger = document.querySelector('.menu');
+const nav = document.querySelector('.menu');
 const navLinks = document.querySelector('.nav-links');
 
-hamburger.addEventListener('click', () => {
+nav.addEventListener('click', () => {
     navLinks.classList.toggle('active');
+});
+
+// Function to delete social_media in future sections
+document.addEventListener("scroll", function () {
+    const homeSection = document.querySelector("#home");
+    const socialMedia = document.querySelector(".social_media");
+
+    const homePosition = homeSection.getBoundingClientRect();
+
+    if (
+        homePosition.bottom >= 0 && homePosition.top <= window.innerHeight
+    ) {
+        socialMedia.classList.remove("hidden");
+    } else {
+        socialMedia.classList.add("hidden");
+    }
 });
